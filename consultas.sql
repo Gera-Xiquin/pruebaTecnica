8f3e70519ff67207eb543db4eb14e4e9cc236f1f
Gerardo Xiquin <geraxiquin@hotmail.com>
2025-02-13 11:49:24 -0600

    consultas de rubros

--- consultas.sql
+++ consultas.sql
@@ -16,13 +16,27 @@ LEFT JOIN (
 ) o ON p.id = o.proyecto_id
 GROUP BY p.id;
 
+
 b)
 
+SET @nombre = 'proyecto 1';
+SET @proyecto = (select p.id from proyectos p where p.nombre = @nombre);
 SELECT 
     r.nombre AS rubro,
-    COALESCE(SUM(d.monto), 0) - COALESCE(SUM(o.monto), 0) AS fondos_disponibles
+   ( COALESCE(d.total_donaciones, 0) -
+    COALESCE(o.total_ordenes, 0) )AS fondo
 FROM rubros r
-LEFT JOIN donaciones d ON r.id = d.rubro_id
-LEFT JOIN ordenes o ON r.id = o.rubro_id
-WHERE r.proyecto_id = (SELECT id FROM proyectos WHERE nombre = 'X')
-GROUP BY r.id;
+LEFT JOIN (
+    SELECT proyecto_id, rubro_id, SUM(monto) AS total_donaciones
+    FROM donaciones
+    WHERE proyecto_id =  @proyecto
+    GROUP BY rubro_id, proyecto_id
+) d ON r.id = d.rubro_id
+LEFT JOIN (
+    SELECT proyecto_id, rubro_id, SUM(monto) AS total_ordenes
+    FROM ordenes_compra
+    WHERE proyecto_id =  @proyecto
+    GROUP BY rubro_id, proyecto_id
+) o ON r.id = o.rubro_id
+WHERE r.proyecto_id = @proyecto;
+
